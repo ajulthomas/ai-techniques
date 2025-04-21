7ca1fb9f76fb05f98cccb1428ebb97bc35f7894e
Ajul Thomas <ajul.thomas@outlook.com>
2025-04-21 13:22:37 +1000

    adds transformer model

--- fake-news-detector/src/roberta_classifier.py
+++ fake-news-detector/src/roberta_classifier.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 
-def load_data(file_path: str) -> pd.DataFrame:
-    df = pd.read_csv(file_path)
-    df.drop(columns=['subject', 'date'], inplace=True)
-    df['news'] = "<title>" + df['title'] + "<content>" + df['text'] + "<end>"
-    return df[['news', 'target']]
+def load_data(df: pd.DataFrame) -> pd.DataFrame:
+    # df = pd.read_csv(file_path)
+    # df.drop(columns=['subject', 'date'], inplace=True)
+    df['text_transformer'] = "<title>" + df['title'] + "<content>" + df['text'] + "<end>"
+    df['label'] = np.where(df['label'] == "FAKE", 1, 0)
+    return df[['text_transformer', 'label']]
 
 def load_model_and_tokenizer(model_name: str):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -39,20 +41,30 @@ def evaluate_model(y_true, y_pred):
     print("F1 Score:", f1_score(y_true, y_pred))
     print("\nClassification Report:\n", classification_report(y_true, y_pred))
 
-def main():
+def roberta_pretrained(df):
     model_name = "hamzab/roberta-fake-news-classification"
     label2id = {"FAKE": 1, "TRUE": 0}
+    # id2label = {1: "FAKE", 0: "TRUE"}
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
     
-    df_model = load_data("./data/cleaned_fake_real_news.csv")
+    df_model = load_data(df)
+    print(df_model.head(5))
+    print("---"*20)
     tokenizer, model = load_model_and_tokenizer(model_name)
     pred_pipeline = create_pipeline(model, tokenizer)
     
-    predicted_labels, confidences = run_predictions(pred_pipeline, df_model["news"].tolist(), label2id)
+    predicted_labels, confidences = run_predictions(pred_pipeline, df_model["text_transformer"].tolist(), label2id)
     
     df_model["predicted_label"] = predicted_labels
     df_model["confidence"] = confidences
-    
-    evaluate_model(df_model["target"], df_model["predicted_label"])
 
-if __name__ == "__main__":
-    main()
+    print(df_model.head(5))
+    print("---"*20)
+
+    # save the df_model to a csv file
+    df_model.to_csv("../output/roberta_predictions.csv", index=False)
+    
+    evaluate_model(df_model["label"], df_model["predicted_label"])
+
