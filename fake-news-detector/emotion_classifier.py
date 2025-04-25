0acffc63088e5966fa3de4466d0b6e4f7c1090e8
ajulthomas <ajul.thomas@outlook.com>
2025-04-25 17:56:53 +1000

    adds sentiment/emotion analysis

---
+++ fake-news-detector/emotion_classifier.py
@@ -0,0 +1,299 @@
+# %% [markdown]
+# # Emotion Detection for news
+
+# %%
+import pandas as pd
+
+
+def data_extract(df, target_value):
+    """
+    Extracts the text and title from the dataframe and  adds a label column.
+    """
+
+    # add a target column to the dataframe
+    df["label"] = target_value
+
+    # return a df copy with text, title and label columns
+    return df[["text", "title", "label"]]
+
+
+DATA_CONFIG = [
+    {
+        "name": "BuzzFeed Fake News",
+        "description": "Fake news articles from BuzzFeed.",
+        "filename": "BuzzFeed_fake_news_content.csv",
+        "extract_func": data_extract,
+        "target_value": "FAKE",
+    },
+    {
+        "name": "BuzzFeed Real News",
+        "description": "Real news articles from BuzzFeed.",
+        "filename": "BuzzFeed_real_news_content.csv",
+        "extract_func": data_extract,
+        "target_value": "TRUE",
+    },
+    {
+        "name": "PolitiFact Fake News",
+        "description": "Fake news articles from PolitiFact.",
+        "filename": "PolitiFact_fake_news_content.csv",
+        "extract_func": data_extract,
+        "target_value": "FAKE",
+    },
+    {
+        "name": "PolitiFact Real News",
+        "description": "Real news articles from PolitiFact.",
+        "filename": "PolitiFact_real_news_content.csv",
+        "extract_func": data_extract,
+        "target_value": "TRUE",
+    },
+    {
+        "name": "Fake News - Kaggle",
+        "description": "Fake news articles from various sources obtained from kaggle.",
+        "filename": "Fake.csv",
+        "extract_func": data_extract,
+        "target_value": "FAKE"
+    },
+    {
+        "name": "True News - Kaggle",
+        "description": "True news articles from various sources obtained from kaggle.",
+        "filename": "True.csv", 
+        "extract_func": data_extract, 
+        "target_value": "TRUE"
+    },
+]
+
+
+def load_merge_data(data_config):
+    """
+    Loads and merges the data from the given config.
+    """
+    dataframes = []
+
+    for config in data_config:
+        filepath = f"./data/{config['filename']}"
+        df = pd.read_csv(filepath)
+        df = config["extract_func"](df, config["target_value"])
+        dataframes.append(df)
+
+    # concatenate all dataframes into one
+    merged_df = pd.concat(dataframes, ignore_index=True)
+
+    return merged_df
+
+
+def basic_sanity_check(df):
+    """
+    Performs a basic sanity check on the dataframe.
+    """
+
+    # sanity check title
+    print("=" * 20)
+    print("{:^20}".format("Sanity Check"))
+    print("=" * 20)
+
+    # check for missing values
+    if df.isnull().values.any():
+        print("Missing values found in the dataframe.")
+        # remove rows with missing values
+        df = df.dropna()
+
+    # check for duplicates
+    if df.duplicated().any():
+        # check for duplicate rows
+        duplicate_rows = df.duplicated().sum()
+        print(f"Duplicate rows found: {duplicate_rows}")
+        # remove duplicate rows
+        df = df.drop_duplicates().reset_index(drop=True)
+        print("Duplicate rows removed.")
+
+    # check for empty strings
+    if (df["text"] == "").any() or (df["title"] == "").any():
+        print("Empty strings found in the dataframe.")
+        # remove rows with empty strings
+        df = df[(df["text"] != "") & (df["title"] != "")].reset_index(drop=True)
+        print("Empty strings removed.")
+
+    # check the shape of the dataframe
+    print(f"Dataframe has {df.shape[0]} rows and {df.shape[1]} columns.")
+    print(f"Dataframe has {df['label'].nunique()} unique labels.")
+
+    print("=" * 20)
+
+
+def save_data(df, path):
+    """
+    Saves the dataframe to a csv file.
+    """
+    df.to_csv(path, index=False)
+    print(f"Data saved to {path}")
+
+
+def generate_raw_data():
+    raw_data = load_merge_data(DATA_CONFIG)
+    basic_sanity_check(raw_data)
+    save_data(raw_data, "./data/raw_data.csv")
+    return raw_data
+
+
+def load_raw_data():
+    """
+    Loads the raw data from the given path.
+    """
+    filepath = "./data/raw_data.csv"
+
+    # check if the file exists, if not generate the raw data
+    try:
+        df = pd.read_csv(filepath)
+        print(f"Raw data loaded from {filepath}.")
+    except FileNotFoundError:
+        print(f"{filepath} not found. Generating raw data...")
+        df = generate_raw_data()
+        print(f"Raw data generated and saved to {filepath}.")
+    return df
+
+# %%
+df_raw = load_raw_data()
+
+# %%
+df_raw.head()
+
+
+# %%
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+# Download necessary resources
+nltk.download("punkt")
+nltk.download("stopwords")
+nltk.download("punkt_tab")
+nltk.download("wordnet")
+
+# Custom stopwords (retain "not" and "can")
+STOP_WORDS = set(stopwords.words("english")) - {"not", "can"}
+
+# custom stopwords
+CUSTOM_STOP_WORDS = {"reuters", "reuter"}
+
+# Combine the two sets of stopwords
+STOP_WORDS = STOP_WORDS.union(CUSTOM_STOP_WORDS)
+
+
+# Function for preprocessing
+def clean_text(text):
+    text = text.lower()  # Lowercase
+    text = re.sub(r"\'t", " not", text)  # Expand "can't" → "can not"
+    text = re.sub(r"@\w+", "", text)  # Remove @name
+    text = re.sub(r"[^a-zA-Z0-9\s\?]", "", text)  # Remove special characters except "?"
+    text = re.sub(r"([^\w\s\?])", "", text)  # Remove punctuations except "?"
+    text = re.sub(
+        r"\s+", " ", text
+    ).strip()  # Remove extra spaces and trailing whitespaces
+    text = re.sub(r"\s+", " ", text)  # Remove extra spaces
+
+    # remove stopwords
+    text = " ".join([word for word in text.split() if word not in STOP_WORDS])
+
+    return text
+
+
+# function to tokenize the text and remove stopwords
+def tokenize_text(text):
+    # Tokenize the text
+    tokens = word_tokenize(text)
+    # Remove stopwords
+    tokens = [word for word in tokens if word not in STOP_WORDS]
+    return tokens
+
+
+def lemmatize_tokens(tokens):   
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(token) for token in tokens]
+
+
+def preprocess_data(df):
+    """
+    Preprocess the data by cleaning the text and removing stopwords.
+    """
+    # clean title
+    df["title"] = df["title"].apply(clean_text)
+
+    # clean text
+    df["text"] = df["text"].apply(clean_text)
+
+    # create a new column called 'news', by merging the 'title' and 'text' columns
+    df["news"] = df["title"] + " " + df["text"]
+
+    # tokenize the news text and remove stopwords
+    df["tokens"] = df["news"].apply(tokenize_text)
+
+    # lemmatize the tokens
+    df["tokens"] = df["tokens"].apply(lemmatize_tokens)
+
+    # merge the tokens into a single string
+    df["cleaned_news"] = df["tokens"].apply(lambda x: " ".join(x))
+
+    # return all columns in order
+    df_cleaned = df[["title", "text", "news", "tokens", "cleaned_news", "label"]]
+
+    return df_cleaned
+
+
+# %%
+df = preprocess_data(df_raw)
+df.head()
+
+# %%
+df_copy = df.copy()
+
+# %%
+
+import pandas as pd
+import numpy as np
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+
+def load_data(df: pd.DataFrame) -> pd.DataFrame:
+    df['text_transformer'] = df['title'] + df['text']
+    df['label'] = np.where(df['label'] == "FAKE", 0, 1)
+    return df[['text_transformer', 'label']]
+
+
+def load_model_and_tokenizer(model_name: str):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    return tokenizer, model
+
+def create_pipeline(model, tokenizer):
+    device = 0 if torch.cuda.is_available() else -1
+    return pipeline(
+        "text-classification",
+        model=model,
+        tokenizer=tokenizer,
+        device=device,
+        max_length=512,
+        truncation=True,
+        padding=True
+    )
+
+# %%
+model_name = "j-hartmann/emotion-english-distilroberta-base"
+
+df_model = load_data(df_copy.copy())
+
+tokenizer, model = load_model_and_tokenizer(model_name)
+emotion_pipeline = create_pipeline(model, tokenizer)
+
+# %%
+predictions = emotion_pipeline(df_model['text_transformer'].tolist(), truncation=True, padding=True)
+predictions.head()
+
+# %%
+
+
+# %%
+
+
+
